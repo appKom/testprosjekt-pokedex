2578f0843d1884f3cf6cabea121268850d04bffc
John <johngot@stud.ntnu.no>
2024-09-08 16:55:24 +0200

    Added types

--- src/Knapp.jsx
+++ src/Knapp.jsx
@@ -1,33 +1,40 @@
-
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 function Knapp(props) {
-    const [verdi, setVerdi] = useState(0);
+  const [verdi, setVerdi] = useState(0);
 
-    const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState(null);
 
-    function hentPokemon() {
-        fetch(props.url).then(response => response.json()).then(data => setPokemon(data));
-    }
+  function hentPokemon() {
+    fetch(props.url)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }
 
-    useEffect(() => {
-        hentPokemon();
-    })
+  useEffect(() => {
+    hentPokemon();
+  });
 
-    function økverdi() {
-        setVerdi(verdi + 1);
-    }
+  function økverdi() {
+    setVerdi(verdi + 1);
+  }
 
-    return (
-        <div className='knappdiv'>
-         <button className='knappelement' onClick={() => økverdi()}>{props.tittel}</button>
-        
-         {pokemon && <img src={pokemon.sprites.front_default}></img>
-         }
-        
+  return (
+    <div className="knappdiv">
+      <button className="knappelement" onClick={() => økverdi()}>
+        {props.tittel}
+      </button>
 
-        </div>
-    )
+      {pokemon && <img src={pokemon.sprites.front_default}></img>}
+      {pokemon &&
+        pokemon.types.map((type, index) => (
+          <span className="typer" key={index}>
+            {type.type.name}
+            {index < pokemon.types.length - 1 ? ", " : ""}
+          </span>
+        ))}
+    </div>
+  );
 }
-    
+
 export default Knapp;
